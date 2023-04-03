a12489d7fcb956020c75a7e26c41899133a52ffc
SattlerRS <16262@edu.szf.at>
2023-04-03 10:06:13 +0200

    Test2

--- src/main/java/com/example/fahradverlei/HelloApplication.java
+++ src/main/java/com/example/fahradverlei/HelloApplication.java
@@ -16,6 +16,7 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
         System.out.println("Hello");
+        System.out.println("Test2");
     }
 
     public static void main(String[] args) {
