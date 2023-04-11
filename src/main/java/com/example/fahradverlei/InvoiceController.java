831c911a0df6cc71d73591d611bc513e3f617eb1
SattlerRS <16262@edu.szf.at>
2023-04-11 15:15:32 +0200

    v1

--- src/main/java/com/example/fahradverlei/InvoiceController.java
+++ src/main/java/com/example/fahradverlei/InvoiceController.java
@@ -19,8 +19,10 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class InvoiceController {
     @FXML
@@ -30,7 +32,7 @@ public class InvoiceController {
     public Label lblDeliverer;
     public Label lblDateDestination;
     public Label lblBike;
-
+    public Label lblTime;
     public PrinterJob printerJob;
 
     public Stage stage;
@@ -43,21 +45,26 @@ public class InvoiceController {
         return fxmlLoader.getController();
     }
 
-    public void printInvoice(Rental item,Bike bike) {
+    public void printInvoice(Rental rental,Bike bike) {
         anchorPaneInvoice.setStyle("-fx-background-color: white;");
-        Customer customer = getCustomerFromCustomerList(item);
+        Customer customer = getCustomerFromCustomerList(rental);
         lblCustomer.setText("Name:\t\t" + customer.getName() + " " + customer.getFirstName() +
                 "\nAnschrift:\t\t" + customer.getStreet() + " " + customer.getHousenumber() +
                 "\nPLZ:\t\t\t" + customer.getPostalCode());
         lblDeliverer.setText("Name:\t\tBikemaster GmbH\nAnschrift:\t\tUrheberverletzungsstraße 45\nPLZ:\t\t\t6969");
-        lblInvoiceNumber.setText(String.valueOf(item.getID()));
+        lblInvoiceNumber.setText(String.valueOf(rental.getID()));
         LocalDate currentDate = LocalDate.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         lblDateDestination.setText(currentDate.format(formatter) + ", Puntigam Links");
-        lblBike.setText("ID:\t\t\t" + bike.getID() +
-                "\nName:\t\t" + bike.getName() +
-                "\nBauart:\t\t" + bike.getDesignType() +
-                "\nPreis / Tag:\t\t" + bike.getPricePerDay());
+        DecimalFormat df = new DecimalFormat("#,##0.00 €");
+        lblBike.setText("ID: " + bike.getID() +
+                "  |  Bezeichnung: " + bike.getName() +
+                "  |  Bauart: " + bike.getDesignType() +
+                "  |  Preis / Tag: " + df.format(bike.getPricePerDay()));
+
+        lblTime.setText("Start Datum:  " + rental.getStartDate().toLocalDate().format(formatter) +
+                "\nEnd Datum:  " + rental.getEndDate().toLocalDate().format(formatter) +
+                "\nTage ausgeliehen: " + ChronoUnit.DAYS.between(rental.getStartDate().toLocalDate(),rental.getEndDate().toLocalDate().plusDays(1)));
 
 
         printerJob = PrinterJob.createPrinterJob();
